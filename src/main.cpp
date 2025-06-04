65ef5139f84b3032dc3b584939108af83bfefa8a
l1nuvv <l1nuvv5@gmail.com>
2025-06-04 22:16:38 +0300

    feat: change window size real-time

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,22 @@
 #include <spdlog/spdlog.h>
 #include <glm/glm.hpp>
 
+struct WindowState {
+    int width;
+    int height;
+};
+
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    WindowState *state = static_cast<WindowState *>(glfwGetWindowUserPointer(window));
+    state->width = width;
+    state->height = height;
+
+    glViewport(0, 0, width, height);
+    spdlog::info("Change window size to: {}x{}", state->width, state->height);
+}
+
 int main()
 {
     // init GLFW
@@ -15,14 +31,17 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // define window size
-    int windowWidth = 800, windowHeight = 800;
+    WindowState windowState{1200, 720};
 
     // create a GLFW window with OpenGL context
-    spdlog::info("Creating GLFW window {}x{}...", windowWidth, windowHeight);
+    spdlog::info("Creating GLFW window {}x{}...", windowState.width, windowState.height);
     spdlog::info("GLM vector size: {}", sizeof(glm::vec3));
     GLFWwindow *window =
-            glfwCreateWindow(windowWidth, windowHeight, "Danek", nullptr, nullptr);
+            glfwCreateWindow(windowState.width, windowState.height, "Danek", nullptr, nullptr);
 
+    glfwSetWindowUserPointer(window, &windowState);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     if (!window)
     {
         // log error and terminate GLFW if window creation failed
@@ -49,25 +68,26 @@ int main()
                  (const char *) glGetString(GL_SHADING_LANGUAGE_VERSION));
 
     // set the viewport to match the window size
-    glViewport(0, 0, windowWidth, windowHeight);
-    spdlog::debug("Viewport set to {}x{}...", windowWidth, windowHeight);
-
-    // set background color to specific color and clear the screen
-    glClearColor(0.07f, 0.13f, 0.17f, 1.0f); // dark blue background now
-    glClear(GL_COLOR_BUFFER_BIT); // clear the color buffer
-    glfwSwapBuffers(window); // display the cleared frame
+    glViewport(0, 0, windowState.width, windowState.height);
+    spdlog::debug("Viewport set to {}x{}...", windowState.width, windowState.height);
 
     // main application loop
     spdlog::info("Entering main loop...");
     while (!glfwWindowShouldClose(window))
     {
+        WindowState *state = static_cast<WindowState *>(glfwGetWindowUserPointer(window));
+
+        // set background color to specific color and clear the screen
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f); // dark blue background for now
+        glClear(GL_COLOR_BUFFER_BIT); // clear the color buffer
+
         glfwPollEvents(); // process pending events like keyboard or mouse input
+        glfwSwapBuffers(window);
     }
 
     // clean up resources before exiting
     spdlog::info("Destroy main loop!");
     glfwDestroyWindow(window);
     glfwTerminate();
-
     return 0;
 }

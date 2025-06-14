283aea20a458448107153187864f891232efc3de
l1nuvv <l1nuvv5@gmail.com>
2025-06-14 23:54:28 +0300

    bug fix in TransformManager::CreateModelMatrix

--- engine/render/TransformManager.cpp
+++ engine/render/TransformManager.cpp
@@ -13,7 +13,7 @@ glm::mat4 TransformManager::CreateModelMatrix(const glm::vec3 &position,
     glm::mat4 model = glm::mat4(1.0f);
 
     model = glm::translate(model, position);
-    model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(1.0f, 0.0f, 0.0f));
+    model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1.0f, 0.0f, 0.0f));
     model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0.0f, 1.0f, 0.0f));
     model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
     model = glm::scale(model, scale);
